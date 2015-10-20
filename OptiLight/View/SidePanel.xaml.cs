c88f1d7f3519154086c2d405bbd969996b42080d
Lasse Iversen <lasseiversen@outlook.dk>
2015-10-20 10:29:43 +0200

    Struktur opsat

--- OptiLight/View/SidePanel.xaml.cs
+++ OptiLight/View/SidePanel.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace OptiLight
+namespace OptiLight.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SidePanel.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SidePanel : UserControl
     {
-        public MainWindow()
+        public SidePanel()
         {
             InitializeComponent();
         }
