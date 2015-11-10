6b2e11cc212c1ebc3dea2bee76f0f65aedaa78a3
NicolaiMogensen <nicolaimogensen@outlook.dk>
2015-11-10 10:12:05 +0100

    Prepare to merge

---
+++ OptiLight/ViewModel/SquareLampViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System;
+
+namespace OptiLight.ViewModel
+{
+    public class SquareLampViewModel : LampViewModel
+    {
+        public SquareLampViewModel(Model.Lamp lamp) : base(lamp) { }
+    }
+}
