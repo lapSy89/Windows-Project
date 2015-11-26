0026c01d8e6de1ff83e5386f08cbc0ba9d69141a
Adam Emil Paltorp Schmitt <s134873@student.dtu.dk>
2015-11-26 21:59:51 +0100

    Canvas Model and ViewModel

---
+++ OptiLight/Model/Canvas.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace OptiLight.Model {
+
+    public class Canvas {
+        
+        // Default values for grid size
+        public int cellSize { get; set; } = 50;
+        public int cellsX { get; set; } = 12;
+        public int cellsY { get; set; } = 10;
+        public int height { get; set; } = 500;
+        public int width { get; set; } = 700;
+    }
+}
